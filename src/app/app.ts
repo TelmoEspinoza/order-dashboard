cc5801fdc9b5d5cf306658ad252fafee82c60d8f
Telmo Espinoza <telmo.espinozag@gmail.com>
2026-04-07 19:49:36 +1000

    ORDERS DASHBOARD SAMPLE 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { OrdersDashboard } from './orders-dashboard/orders-dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [OrdersDashboard ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
